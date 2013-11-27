4c94a4e0e4c1f949eb81855ceedbe214728f1543
kumano ayumi <kumayuzuki@gmail.com>
2013-11-27 10:58:14 +0900

    css2sass

---
+++ config.rb
@@ -0,0 +1,23 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+http_stylesheets_path = '/css'
+http_images_path      = '/img'
+http_javascripts_path = '/js'
+
+css_dir = "docs/css"
+sass_dir = "docs/sass"
+images_dir = "docs/img"
+javascripts_dir = "docs/js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+preferred_syntax = :sass
